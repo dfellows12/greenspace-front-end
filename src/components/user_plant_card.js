135e3fd8cd5689619e86da9ac3789b4183e79947
David Fellows <dfellows12@gmail.com>
2019-12-17 17:03:04 -0500

    Added calendar form

--- src/components/user_plant_card.js
+++ src/components/user_plant_card.js
@@ -1,22 +1,70 @@
-import React from "react";
-import { Card, Image, Icon, Button } from 'semantic-ui-react'
+import React, { Component } from "react"
+import { Form, Card, Image, Icon, Button } from 'semantic-ui-react'
 import { Link } from "react-router-dom"
 import { connect } from "react-redux";
 import { deletingUserPlant } from '../redux/actions/user_plant_actions'
+import { DateInput, TimeInput, DateTimeInput, DatesRangeInput } from 'semantic-ui-calendar-react';
 
-const UserPlantCard = props => {
+
+class UserPlantCard extends Component {
+    state = {
+        date: '',
+        time: '',
+        dateTime: '',
+        datesRange: ''
+      };
+
+    handleChange = (event, {name, value}) => {
+        if (this.state.hasOwnProperty(name)) {
+          this.setState({ [name]: value });
+        }
+      }
+
+    render() {
     return(
         <div>
             <Card>
-                <Image src={props.user_plant.image_url} wrapped ui={false} />
+            <Form>
+        <DateInput
+          name="date"
+          placeholder="Date"
+          value={this.state.date}
+          iconPosition="left"
+          onChange={this.handleChange}
+        />
+        <TimeInput
+          name="time"
+          placeholder="Time"
+          value={this.state.time}
+          iconPosition="left"
+          onChange={this.handleChange}
+        />
+        <DateTimeInput
+          name="dateTime"
+          placeholder="Date Time"
+          value={this.state.dateTime}
+          iconPosition="left"
+          onChange={this.handleChange}
+        />
+        <DatesRangeInput
+          name="datesRange"
+          placeholder="From - To"
+          value={this.state.datesRange}
+          iconPosition="left"
+          onChange={this.handleChange}
+        />
+      </Form>
+
+                <Image src={this.props.user_plant.image_url} wrapped ui={false} />
                 <Card.Content>
-                    <Card.Header>{props.user_plant.name}</Card.Header>
+                    <Card.Header>{this.props.user_plant.name}</Card.Header>
                     <Card.Meta>
-                        <span className='date'>{props.user_plant.scientific_name}</span>
+                        <span className='date'>{this.props.user_plant.scientific_name}</span>
                     </Card.Meta>
                     <Card.Description>
-                        {props.user_plant.info}
+                        {this.props.user_plant.info}
                     </Card.Description>
+                    
                 </Card.Content>
                 <Card.Content extra>
                     <a>
@@ -25,12 +73,15 @@ const UserPlantCard = props => {
                     </a>
                 </Card.Content>
                 <Button onClick={() => {
-                    props.deleteUserPlant(props.user_plant)
+                    this.props.deleteUserPlant(this.props.user_plant)
+                }}>Edit plant</Button>
+                <Button onClick={() => {
+                    this.props.deleteUserPlant(this.props.user_plant)
                 }}>Remove plant</Button>
             </Card>
         </div>
     )
-}
+}}
 
 const mapDispatchToProps = dispatch => ({
     deleteUserPlant: (userPlantInfo) => {dispatch(deletingUserPlant(userPlantInfo))}
